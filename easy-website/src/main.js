6702aced2058f210905a7b8c6e871d1a8b779630
meryemkutlu <meryem.kutlu13@gmail.com>
2023-12-12 23:48:00 +0300

    use react-ikon and make a simple website

---
+++ easy-website/src/main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./main.css";
+
+function main() {
+  return (
+    <div className="site-info">
+      <div className="title">Meryem Kutlu</div>
+      <div className="sub-title">Front-End Developer </div>
+    </div>
+  );
+}
+
+export default main;
