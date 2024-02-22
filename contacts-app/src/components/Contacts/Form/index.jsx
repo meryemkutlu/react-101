3d4162fdb750d91fc1e047e267fad24df9b6048c
meryemkutlu <meryem.kutlu13@gmail.com>
2024-02-22 16:51:14 +0300

    Store data using useState and clear form with useEffect

--- contacts-app/src/components/Contacts/Form/index.jsx
+++ contacts-app/src/components/Contacts/Form/index.jsx
@@ -1,15 +1,48 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-function Form() {
+const initialFormValues = { fullname: "", phone_number: "" };
+
+function Form({ addContact, contacts }) {
+  const [form, setForm] = useState(initialFormValues);
+
+  useEffect(() => {
+    setForm(initialFormValues);
+  }, [contacts]);
+
+  const onChangeInput = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (form.fullname === "" || form.phone_number === "") {
+      return false;
+    }
+    addContact([...contacts, form]);
+  };
+
   return (
-    <div>
+    <form onSubmit={onSubmit}>
       <div>
-        <input name="fullname" placeholder="Full Name" />
+        <input
+          name="fullname"
+          placeholder="Full Name"
+          value={form.fullname}
+          onChange={onChangeInput}
+        />
       </div>
       <div>
-        <input name="phone_number" placeholder="Phone Number" />
+        <input
+          name="phone_number"
+          value={form.phone_number}
+          placeholder="Phone Number"
+          onChange={onChangeInput}
+        />
       </div>
-    </div>
+      <div>
+        <button>Add</button>
+      </div>
+    </form>
   );
 }
 
