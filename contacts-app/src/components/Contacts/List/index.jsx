7cf08e64489d5dd4a9d3565ce5be8d0dd4191fee
meryemkutlu <meryem.kutlu13@gmail.com>
2024-02-23 14:30:14 +0300

    Filtering Process add

--- contacts-app/src/components/Contacts/List/index.jsx
+++ contacts-app/src/components/Contacts/List/index.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import { useState } from "react";
 
 function List({ contacts }) {
+  const [filterText, setFilterText] = useState("");
+
+  const filtered = contacts.filter((item) => {
+    return Object.keys(item).some((key) =>
+      item[key].toString().toLowerCase().includes(filterText.toLowerCase())
+    );
+  });
+
   return (
     <div>
+      <input
+        placeholder="Filter contact"
+        value={filterText}
+        onChange={(e) => setFilterText(e.target.value)}
+      />
+
       <ul>
-        {contacts.map((contacts, i) => (
+        {filtered.map((contacts, i) => (
           <li key={i}> {contacts.fullname} </li>
         ))}
       </ul>
--- contacts-app/src/components/Contacts/index.jsx
+++ contacts-app/src/components/Contacts/index.jsx
@@ -3,7 +3,20 @@ import Form from "./Form";
 import List from "./List";
 
 function Contacts() {
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState([
+    {
+      fullname: "John",
+      phone: "1234567890",
+    },
+    {
+      fullname: "John Smith",
+      phone: "123456789990",
+    },
+    {
+      fullname: "Smith",
+      phone: "123456788990",
+    },
+  ]);
   useEffect(() => {
     console.log(contacts);
   }, [contacts]);
